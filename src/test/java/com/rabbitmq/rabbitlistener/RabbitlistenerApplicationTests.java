52aa76b47e9830099d0a55fe233d782fa46c24ea
mliunardo <mliunardo@gmail.com>
2020-04-16 20:00:06 +0800

    Initial Commit

---
+++ src/test/java/com/rabbitmq/rabbitlistener/RabbitlistenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.rabbitlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitlistenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
