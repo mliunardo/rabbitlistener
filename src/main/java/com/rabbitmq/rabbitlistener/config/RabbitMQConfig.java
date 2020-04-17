a7c14419f079f69b2cffa21f9904b97ce1bf176d
mliunardo <mliunardo@gmail.com>
2020-04-17 10:31:08 +0800

    [RabbitMQ] Section 4: 23. Testing Out the Listener

---
+++ src/main/java/com/rabbitmq/rabbitlistener/config/RabbitMQConfig.java
@@ -0,0 +1,42 @@
+package com.rabbitmq.rabbitlistener.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.rabbitmq.rabbitlistener.component.RabbitMQMessageListener;
+
+
+@Configuration
+public class RabbitMQConfig {
+
+	private static final String MY_QUEUE = "MyQueue";
+	
+	@Bean
+	public Queue myQueue() {
+		return new Queue(MY_QUEUE, true);
+	}
+
+	@Bean
+	public ConnectionFactory connectionFactory() {
+		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory("localhost");
+		cachingConnectionFactory.setUsername("guest");
+		cachingConnectionFactory.setPassword("guest");
+		return cachingConnectionFactory;
+	}
+	
+	@Bean
+	public MessageListenerContainer messageListenerContainer() {
+		SimpleMessageListenerContainer simpleMessageListenerContainer = new SimpleMessageListenerContainer();
+		simpleMessageListenerContainer.setConnectionFactory(connectionFactory());
+		simpleMessageListenerContainer.setQueues(myQueue());
+		simpleMessageListenerContainer.setMessageListener(new RabbitMQMessageListener());
+		return simpleMessageListenerContainer;
+	}
+	
+	
+}
